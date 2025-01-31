efc4b0f5b8c54598d2c4181de3da64ef09be1d63
Inteside <2373850947@qq.com>
2025-01-31 15:02:54 +0800

    feat: Add Java path retrieval functionality
    - 在Rust后台实现“get_java_path”方法
    - 在Vue前端添加新按钮和方法以获取Java主路径
    - 更新Tauri调用处理程序以包含新命令
    - 使用描述性控制台消息增强日志记录

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,10 +10,11 @@ mod Setting;
 use api::login::get_code;
 use module::download::dwl_main::get_version_manifest;
 use module::download::dwl_main::dwl_version_manifest;
+use utils::get_java_path::get_java_path;
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![get_code, get_version_manifest,dwl_version_manifest])
+        .invoke_handler(tauri::generate_handler![get_code, get_version_manifest,dwl_version_manifest, get_java_path])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
